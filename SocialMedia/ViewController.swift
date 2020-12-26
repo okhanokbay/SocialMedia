3f0dc202d2d750c4906e31c9ce9809a31771de50
Okhan Okbay <okhanokbay@users.noreply.github.com>
2020-12-26 14:23:42 +0300

    Initial commit

---
+++ SocialMedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialMedia
+//
+//  Created by Okhan Okbay on 26.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
